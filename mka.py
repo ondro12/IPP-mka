884f326fc2b91cca7ca6e5d0a08e098480b0443a
Matúš Ondris <xondri04@stud.fit.vutbr.cz>
2016-04-19 11:01:10 +0200

    Update mka.py

--- mka.py
+++ mka.py
@@ -10,9 +10,9 @@ napoveda = "Skript sluziaci na minimalizaciu konecneho automatu v pythone.\n\
        --help\n\
               Vypis napovedy skriptu.\n\
        --input=filename\n\
-              Zadany vstupny subor. Pokial  nieje zadany, ako vstup sa pouzije stdin\n\
+              Zadany vstupny subor relativnou, alebo absolutnou cestou. Pokial  nieje zadany, ako vstup sa pouzije stdin\n\
        --output=filname\n\
-              Vystupni subor. Pokial nieje zadany, ako vystup sa pouzije stdout\n\
+              Vystupni subor relativnou, alebo absolutnou cestou. Pokial nieje zadany, ako vystup sa pouzije stdout\n\
        -f, --find-non-finishing\n\
               Hlada sa neukoncujuci stav zadaneho WSPFA.\n\
        -m, --minimize\n\
@@ -43,34 +43,38 @@ def chybovy_vypis(e_id='e_unknown'):
 	else:
 		sys.exit(0)
 
+# trieda argumenty, obsahuje zaznamy o zadanych argumentoch
 class Arguments:
 	def __init__(self):
 		self.vstup = sys.stdin
 		self.vystup = sys.stdout
-		self.f 			= None # find non-finishing
-		self.m 			= None # minimze
-		self.i 			= None # case-insensitive
-		self.wht		= None # white-char bonus
-		self.rlo		= None # rules-only bonus
-		self.mst		= None # analyze string bonus
-		self.mws 		= None # well-specified final automat bonus
-		self.neznama	= None
+		self.f 			= None # argument -f
+		self.m 			= None # argument -m
+		self.i 			= None # argument -i
+		self.wht		= None # bonus wth
+		self.rlo		= None # bonus rlo
+		self.mst		= None # bonus mst
+		self.mws 		= None # bonus mws
+		self.neznama		= None
 
+# Spracovanie parametru imput , zistenie ci sa jedna o subor , alebo stdin
 	def nastav_vstup(self, argumenty):
 		vstupny_subor = argumenty[argumenty.find("--input=")+len("--input="):]
 		try:
+# Pokus otvorit subor pre citanie 			
 			self.vstup = open(vstupny_subor, 'r')
-		except: #cath 'em all :-D
+		except: 
 			chybovy_vypis('e_in')
 
-
+# Spracovanie parametru output , zistenie ci sa jedna o subor , alebo stdout
 	def nastav_vystup(self, argumenty):
 		vystupny_subor = argumenty[argumenty.find("--output=")+len("--output="):]
 		try:
+# Pokus otvorit subor pre zapis 			
 			self.vystup = open(vystupny_subor, 'w')
 		except:
 			chybovy_vypis('e_in')
-
+# Spracovanie parametru anamyze-string 
 	def nastav_hladany(self,argumenty):
 		mst_text = argumenty[argumenty.find("--analyze-string=")+len("--analyze-string="):]
 		if mst_text[0] == "\"" or mst_text[-1] == "\"":
@@ -86,10 +90,9 @@ class Arguments:
 		print("\tminimize:", self.m)
 		print("\tcase-insensitive:", self.i)
 		print("\tuknown(optional):", self.neznama)
-	#show
 
+# kontrola, ci boli argumenty zadane spravne , ci sa nevyskytuje nepovolena kombinacia
 	def kontrola_argumentov(self):
-		""" Check if there are all of the rules OK """
 		if self.f and self.m:
 			return False
 		elif (self.f or self.m) and self.mst:
@@ -98,7 +101,6 @@ class Arguments:
 			return False
 
 		return True
-	#kontrola_argumentov
 
 if __name__ == '__main__':
 	argumenty = Arguments()
@@ -113,42 +115,43 @@ if __name__ == '__main__':
 
 		if p == "-f" or p == "--find-non-finishing":
 			argumenty.f = True
-		elif p == "-m" or p == "--minimize":
-			argumenty.m = True
-		elif p == "-i" or p == "--case-insensitive":
-			argumenty.i = True
 		elif p.find("--input=") != -1:
-			argumenty.nastav_vstup(p)
+			argumenty.nastav_vstup(p
 		elif p.find("--output=") != -1:
 			argumenty.nastav_vystup(p)
-		elif p == "-w" or p == "--white-char":
-			argumenty.wht = True
+		elif p == "-m" or p == "--minimize":
+			argumenty.m = True
+			
+		elif p == "-i" or p == "--case-insensitive":
+			argumenty.i = True
 		elif p == "-r" or p == "--rules-only":
-			argumenty.rlo = True
+			argumenty.rlo = True	
 		elif p.find("--analyze-string=") != -1:
 			argumenty.nastav_hladany(p)
+		elif p == "-w" or p == "--white-char":
+			argumenty.wht = True
 		elif p == "--wsfa":
 			argumenty.mws = True
 		else:
 			argumenty.neznama = True
-	#for
 
 	if not argumenty.kontrola_argumentov():
 		chybovy_vypis("e_arg")
 
-	#read input
+#citanie vstupu , nacitanie do pamati
 	try:
 		citany_text = argumenty.vstup.read()
 	except:
 		chybovy_vypis('e_in')
 
+# ak bol zadany parameter i , vsetky znaky zmen na male teda A na a , ...
 	if argumenty.i:
 		citany_text = citany_text.lower()
 
-	#create instance of Analysis object and analyze it
+#vytvorenie instancie Analysis objektu a jej analyza
 	ana = analysis.Analysis(citany_text, argumenty.wht, argumenty.i)
 	code, machine = 0, ""
-	#in case of rlo bonus use different analyze function
+#v pripade, ze je pozadovane previest rlo bonus, pouzije sa odlisna funkcia na analyzovanie
 	if argumenty.rlo:
 		code, machine = ana.analyze_by_rules()
 	else:
@@ -157,23 +160,23 @@ if __name__ == '__main__':
 		print(machine, file=sys.stderr)
 		chybovy_vypis('e_ana')
 
-	#reamove duplicates
+#odstranenie opakujucich sa
 	machine._nahrad_opakujucel_()
 
-	#over (look for semantic errors)
+#hladanie semantickych chyb
 	code, msg = machine.over()
 	if code != 0:
 		chybovy_vypis('e_sem')
 
-	# if mws is enabled, try convert fsm into wsfa
+# ak je zadany parameter mws, pokusi sa previest fsm into wsfa
 	if argumenty.mws:
 		machine._preved_na_wsfa_()
 
-	#check if it is wsfa
+#kontrola , ci sa jedna o wsfa
 	if not machine._je_wsfa_():
 		chybovy_vypis('e_wsfa')
 
-	#chech if there is mst and if so try string
+#kontola , ci je zadany mst a ak ano tak pokus previest na pozadovany retazec
 	if argumenty.mst:
 		if machine._analyzuj_retazec_(argumenty.mst):
 			print("1", sep="", end="\n", file=argumenty.vystup)
@@ -182,23 +185,23 @@ if __name__ == '__main__':
 		chybovy_vypis('e_ok')
 
 	if argumenty.f:
-		#find non-finishing
+#najdenie neukoncujucich stavov
 		non_finishing = machine._neukoncujuce_stavy_()
 		if len(non_finishing) > 0:
 			print(non_finishing[0], file=argumenty.vystup, end="")
 		chybovy_vypis('e_ok')
 
 	if argumenty.m:
-		#minimize
+#minimalizacia
 		machine._minimalizuj_()
-		#write it
+#vypis
 		machine._vypis_minimalizovany_(argumenty.vystup)
 		chybovy_vypis('e_ok')
 
-	# if we did nothing so far, print only wsfa
+# ak nebol nutny ziaden prevod , ani zasah vypise sa le wsfa
 	machine._vypis_wsfa_(argumenty.vystup)
 
-	# cleaning up and exiting
+# zatvorenie suborov a ukoncenie skriptu
 	argumenty.vystup.close()
 	argumenty.vstup.close()
 	chybovy_vypis('e_ok')
